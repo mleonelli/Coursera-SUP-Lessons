175b89940aefaec1ea4d27a9cc539faa4d1fa154
Mauro Leonelli <mleonelli@gmail.com>
2013-07-04 12:43:10 +0200

    Update 4-cp.sh

--- 4a/4-cp.sh
+++ 4a/4-cp.sh
@@ -1,12 +1,12 @@
-#!/usr/bin/en bash
+#!/usr/bin/env bash
 
 cp --help
 cp demo.txt demo2.txt
 cp -v demo.txt demo3.txt # verbose copy
 cp -a demo.txt demo-archive.txt # archival exact copy
-ll --full-time demo* # note timestamps of demo/demo-archive
+ls -alrth --full-time demo* # note timestamps of demo/demo-archive
 echo "a new file" > asdf.txt
 cat demo.txt
 cp asdf.txt demo.txt
 cat demo.txt # cp just clobbered the file
-alias cp=’cp -i’ # set cp to be interactive by default
+#alias cp=’cp -i’ # set cp to be interactive by default
